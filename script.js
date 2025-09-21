d9086304194f9ab0f11108aa107e775e1b77c225
Ferdinand <149224161+Ferdasyz@users.noreply.github.com>
2025-09-21 18:24:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// File ini kosong secara default, tapi bisa diisi dengan kode JS sesuai kebutuhan
+// Contoh sederhana:
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website portofolio berhasil dimuat!');
+});
